2cbd99f4c73415359e4ea682ceba84ed9a7e5db7
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-11 18:13:27 +0200

    reset name

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular Project 2025';
+  title = 'Angular Project: August-2025';
 }
--- server/app.js
+++ server/app.js
@@ -9,6 +9,15 @@ const app = express()
 app.use(cors());
 const httpServer = createServer(app);
 
+app.use(express.json());
+
+app.get('/api/photos', (req, res) => {
+    res.json([
+        {id: 1, title: 'Photo 1'},
+        {id: 2, title: 'Photo 2'},
+    ])
+})
+
 
 httpServer.listen(port, () => {
     console.log(`Server is listening on ${port} port!`);
