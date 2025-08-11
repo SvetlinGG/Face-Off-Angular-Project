6a2f4b8f849c2ce8379cb4ca24cedf6c562da67a
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-11 18:06:20 +0200

    photo service

--- client/src/app/services/photo.service.ts
+++ client/src/app/services/photo.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
+  private apiUrl = 'http://localhost:3000/api/photos';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPhotos(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  addPhoto(photoData: FormData): Observable<any> {
+    return this.http.post(this.apiUrl, photoData);
+  }
+
+  updatePhoto(id: string, photoData: FormData): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, photoData);
+  }
+
+  deletePhoto(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`)
+  }
 }
